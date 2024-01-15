9e4dd05667f6e64d73816659d9c25fe5d81197f8
Nathan Zhang <nzhang32@gmail.com>
2024-01-15 12:13:29 -0800

    Repeat updates

--- src/templates/repeat.rs
+++ src/templates/repeat.rs
@@ -172,150 +172,156 @@ where
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::simulation::Program;
-//     use crate::templates::sam::repeat::Repsiggen;
-//     use crate::{
-//         context::{checker_context::CheckerContext, generator_context::GeneratorContext},
-//         repsig_vec,
-//         templates::sam::primitive::Token,
-//         token_vec,
-//     };
+#[cfg(test)]
+mod tests {
+    use dam::simulation::*;
+    use dam::utility_contexts::{CheckerContext, GeneratorContext};
 
-//     use super::RepSigGenData;
-//     use super::Repeat;
-//     use super::RepeatData;
-//     use super::RepeatSigGen;
+    use crate::templates::primitive::{Repsiggen, Token};
+    use crate::{repsig_vec, token_vec};
 
-//     #[test]
-//     fn repeat_2d_test() {
-//         let in_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
-//         let in_repsig = || {
-//             repsig_vec!("R", "R", "R", "S", "R", "R", "R", "S", "R", "S", "R", "R", "S", "D")
-//                 .into_iter()
-//         };
-//         let out_ref = || {
-//             token_vec!(u32; u32; 0, 0, 0, "S0", 1, 1, 1, "S1", 2, "S1", 3, 3, "S2", "D").into_iter()
-//         };
-//         repeat_test(in_ref, in_repsig, out_ref);
-//     }
+    use super::RepSigGenData;
+    use super::Repeat;
+    use super::RepeatData;
+    use super::RepeatSigGen;
 
-//     #[test]
-//     fn repeat_1d_test() {
-//         let in_ref = || token_vec!(u32; u32; 0, 1, 2, "S0", "D").into_iter();
-//         let in_repsig = || repsig_vec!("R", "R", "S", "R", "S", "R", "S", "D").into_iter();
-//         let out_ref = || token_vec!(u32; u32; 0, 0, "S0", 1, "S0", 2, "S1", "D").into_iter();
-//         repeat_test(in_ref, in_repsig, out_ref);
-//     }
+    #[test]
+    fn repeat_2d_test() {
+        let in_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
+        let in_repsig = || {
+            repsig_vec!("R", "R", "R", "S", "R", "R", "R", "S", "R", "S", "R", "R", "S", "D")
+                .into_iter()
+        };
+        let out_ref = || {
+            token_vec!(u32; u32; 0, 0, 0, "S0", 1, 1, 1, "S1", 2, "S1", 3, 3, "S2", "D").into_iter()
+        };
+        repeat_test(in_ref, in_repsig, out_ref);
+    }
 
-//     #[test]
-//     fn repsiggen_2d_test() {
-//         let in_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
-//         let out_repsig = || repsig_vec!("R", "R", "S", "R", "S", "R", "S", "D").into_iter();
-//         repsiggen_test(in_ref, out_repsig);
-//     }
+    #[test]
+    fn repeat_1d_test() {
+        let in_ref = || token_vec!(u32; u32; 0, 1, 2, "S0", "D").into_iter();
+        let in_repsig = || repsig_vec!("R", "R", "S", "R", "S", "R", "S", "D").into_iter();
+        let out_ref = || token_vec!(u32; u32; 0, 0, "S0", 1, "S0", 2, "S1", "D").into_iter();
+        repeat_test(in_ref, in_repsig, out_ref);
+    }
 
-//     #[test]
-//     fn full_repeat_2d_test() {
-//         let in_ref = || token_vec!(u32; u32; 0, 1, 2, "S0", "D").into_iter();
-//         let in_repsig_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
-//         let out_ref = || token_vec!(u32; u32; 0, 0, "S0", 1, "S0", 2, "S1", "D").into_iter();
-//         // let in_repsig = || repsig_vec!("R", "R", "S", "R", "S", "R", "S", "D").into_iter();
-//         full_repeat_test(in_repsig_ref, in_ref, out_ref);
-//     }
+    #[test]
+    fn repsiggen_2d_test() {
+        let in_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
+        let out_repsig = || repsig_vec!("R", "R", "S", "R", "S", "R", "S", "D").into_iter();
+        repsiggen_test(in_ref, out_repsig);
+    }
 
-//     fn full_repeat_test<IRT1, IRT2, ORT>(
-//         in_ref_sig: fn() -> IRT1,
-//         in_ref: fn() -> IRT2,
-//         out_ref: fn() -> ORT,
-//     ) where
-//         IRT1: Iterator<Item = Token<u32, u32>> + 'static,
-//         IRT2: Iterator<Item = Token<u32, u32>> + 'static,
-//         ORT: Iterator<Item = Token<u32, u32>> + 'static,
-//     {
-//         let mut parent = ProgramBuilder::default();
-//         let (in_repsig_ref_sender, in_repsig_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let (out_repsig_sender, out_repsig_receiver) = parent.unbounded::<Repsiggen>();
-//         let repsig_data = RepSigGenData::<u32, u32> {
-//             input: in_repsig_ref_receiver,
-//             out_repsig: out_repsig_sender,
-//         };
-//         let repsig = RepeatSigGen::new(repsig_data);
+    #[test]
+    fn full_repeat_2d_test() {
+        let in_ref = || token_vec!(u32; u32; 0, 1, 2, "S0", "D").into_iter();
+        let in_repsig_ref = || token_vec!(u32; u32; 0, 1, "S0", 2, "S0", 3, "S1", "D").into_iter();
+        let out_ref = || token_vec!(u32; u32; 0, 0, "S0", 1, "S0", 2, "S1", "D").into_iter();
 
-//         let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let (out_ref_sender, out_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let data = RepeatData::<u32, u32> {
-//             in_ref: in_ref_receiver,
-//             in_repsig: out_repsig_receiver,
-//             out_ref: out_ref_sender,
-//         };
-//         let rep = Repeat::new(data);
-//         let repsig_gen = GeneratorContext::new(in_ref_sig, in_repsig_ref_sender);
-//         let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
-//         // let mut gen2 = GeneratorContext::new(in_repsig, in_repsig_sender);
-//         let val_checker = CheckerContext::new(out_ref, out_ref_receiver);
-//         parent.add_child(gen1);
-//         parent.add_child(repsig_gen);
-//         parent.add_child(val_checker);
-//         parent.add_child(rep);
-//         parent.add_child(repsig);
-//         parent.init();
-//         parent.run();
-//     }
+        full_repeat_test(in_repsig_ref, in_ref, out_ref);
+    }
 
-//     fn repeat_test<IRT1, IRT2, ORT>(
-//         in_ref: fn() -> IRT1,
-//         in_repsig: fn() -> IRT2,
-//         out_ref: fn() -> ORT,
-//     ) where
-//         IRT1: Iterator<Item = Token<u32, u32>> + 'static,
-//         IRT2: Iterator<Item = Repsiggen> + 'static,
-//         ORT: Iterator<Item = Token<u32, u32>> + 'static,
-//     {
-//         let mut parent = ProgramBuilder::default();
-//         let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let (in_repsig_sender, in_repsig_receiver) = parent.unbounded::<Repsiggen>();
-//         let (out_ref_sender, out_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let data = RepeatData::<u32, u32> {
-//             in_ref: in_ref_receiver,
-//             in_repsig: in_repsig_receiver,
-//             out_ref: out_ref_sender,
-//         };
-//         let rep = Repeat::new(data);
-//         let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
-//         let gen2 = GeneratorContext::new(in_repsig, in_repsig_sender);
-//         let val_checker = CheckerContext::new(out_ref, out_ref_receiver);
+    fn full_repeat_test<IRT1, IRT2, ORT>(
+        in_ref_sig: fn() -> IRT1,
+        in_ref: fn() -> IRT2,
+        out_ref: fn() -> ORT,
+    ) where
+        IRT1: Iterator<Item = Token<u32, u32>> + 'static,
+        IRT2: Iterator<Item = Token<u32, u32>> + 'static,
+        ORT: Iterator<Item = Token<u32, u32>> + 'static,
+    {
+        let mut parent = ProgramBuilder::default();
+        let (in_repsig_ref_sender, in_repsig_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let (out_repsig_sender, out_repsig_receiver) = parent.unbounded::<Repsiggen>();
+        let repsig_data = RepSigGenData::<u32, u32> {
+            input: in_repsig_ref_receiver,
+            out_repsig: out_repsig_sender,
+        };
+        let repsig = RepeatSigGen::new(repsig_data);
 
-//         parent.add_child(gen1);
-//         parent.add_child(gen2);
-//         parent.add_child(val_checker);
-//         parent.add_child(rep);
-//         parent.init();
-//         parent.run();
-//     }
+        let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let (out_ref_sender, out_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let data = RepeatData::<u32, u32> {
+            in_ref: in_ref_receiver,
+            in_repsig: out_repsig_receiver,
+            out_ref: out_ref_sender,
+        };
+        let rep = Repeat::new(data);
+        let repsig_gen = GeneratorContext::new(in_ref_sig, in_repsig_ref_sender);
+        let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
 
-//     fn repsiggen_test<IRT, ORT>(in_ref: fn() -> IRT, out_repsig: fn() -> ORT)
-//     where
-//         IRT: Iterator<Item = Token<u32, u32>> + 'static,
-//         ORT: Iterator<Item = Repsiggen> + 'static,
-//     {
-//         let mut parent = ProgramBuilder::default();
-//         let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
-//         let (out_repsig_sender, out_repsig_receiver) = parent.unbounded::<Repsiggen>();
-//         let data = RepSigGenData::<u32, u32> {
-//             input: in_ref_receiver,
-//             out_repsig: out_repsig_sender,
-//         };
+        let val_checker = CheckerContext::new(out_ref, out_ref_receiver);
+        parent.add_child(gen1);
+        parent.add_child(repsig_gen);
+        parent.add_child(val_checker);
+        parent.add_child(rep);
+        parent.add_child(repsig);
+        let executed = parent
+            .initialize(InitializationOptions::default())
+            .unwrap()
+            .run(RunOptions::default());
+        dbg!(executed.elapsed_cycles());
+    }
 
-//         let repsig = RepeatSigGen::new(data);
-//         let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
-//         let val_checker = CheckerContext::new(out_repsig, out_repsig_receiver);
+    fn repeat_test<IRT1, IRT2, ORT>(
+        in_ref: fn() -> IRT1,
+        in_repsig: fn() -> IRT2,
+        out_ref: fn() -> ORT,
+    ) where
+        IRT1: Iterator<Item = Token<u32, u32>> + 'static,
+        IRT2: Iterator<Item = Repsiggen> + 'static,
+        ORT: Iterator<Item = Token<u32, u32>> + 'static,
+    {
+        let mut parent = ProgramBuilder::default();
+        let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let (in_repsig_sender, in_repsig_receiver) = parent.unbounded::<Repsiggen>();
+        let (out_ref_sender, out_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let data = RepeatData::<u32, u32> {
+            in_ref: in_ref_receiver,
+            in_repsig: in_repsig_receiver,
+            out_ref: out_ref_sender,
+        };
+        let rep = Repeat::new(data);
+        let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
+        let gen2 = GeneratorContext::new(in_repsig, in_repsig_sender);
+        let val_checker = CheckerContext::new(out_ref, out_ref_receiver);
 
-//         parent.add_child(gen1);
-//         parent.add_child(val_checker);
-//         parent.add_child(repsig);
-//         parent.init();
-//         parent.run();
-//     }
-// }
+        parent.add_child(gen1);
+        parent.add_child(gen2);
+        parent.add_child(val_checker);
+        parent.add_child(rep);
+        let executed = parent
+            .initialize(InitializationOptions::default())
+            .unwrap()
+            .run(RunOptions::default());
+        dbg!(executed.elapsed_cycles());
+    }
+
+    fn repsiggen_test<IRT, ORT>(in_ref: fn() -> IRT, out_repsig: fn() -> ORT)
+    where
+        IRT: Iterator<Item = Token<u32, u32>> + 'static,
+        ORT: Iterator<Item = Repsiggen> + 'static,
+    {
+        let mut parent = ProgramBuilder::default();
+        let (in_ref_sender, in_ref_receiver) = parent.unbounded::<Token<u32, u32>>();
+        let (out_repsig_sender, out_repsig_receiver) = parent.unbounded::<Repsiggen>();
+        let data = RepSigGenData::<u32, u32> {
+            input: in_ref_receiver,
+            out_repsig: out_repsig_sender,
+        };
+
+        let repsig = RepeatSigGen::new(data);
+        let gen1 = GeneratorContext::new(in_ref, in_ref_sender);
+        let val_checker = CheckerContext::new(out_repsig, out_repsig_receiver);
+
+        parent.add_child(gen1);
+        parent.add_child(val_checker);
+        parent.add_child(repsig);
+        let executed = parent
+            .initialize(InitializationOptions::default())
+            .unwrap()
+            .run(RunOptions::default());
+        dbg!(executed.elapsed_cycles());
+    }
+}
